874d5de932c5bc0ec894c4fc69517087957d53b7
Abhishek Dangol <abhi.dangol@gmail.com>
2026-01-25 13:33:03 -0600

    Initial Commit

---
+++ CinematiCam/CinematiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinematiCamApp.swift
+//  CinematiCam
+//
+//  Created by Abhishek Dangol on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CinematiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinematiCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CinematiCam
+//
+//  Created by Abhishek Dangol on 1/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
